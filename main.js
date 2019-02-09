045daaaeb8cc11d400dcad10b2d4652984faffbe
SimaxSaab <minorea@yandex.ru>
2019-02-09 12:27:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var timer,              //  игровой таймер
     numOfSuccess = 0,   //  количество правильных ответов
     numOfAnswers = 6,   //  всего ответов +1
+	elem,
     swither;            //  защита от многократных попыток выбора ответа
-
+	
+window.onload = run;
+	
+function run()
+	{
+		elem = document.querySelector('a');		//	выбрать ссылку
+		elem.addEventListener('click',Main);	//	и пповесить на неё слушатель
+		elem.style.color = 'blue';				//	а также покрасить в link
+	}
+	
 	function Main()
 		{
 			numOfAnswers--;                 // 5 заданий
@@ -39,24 +49,25 @@ var timer,              //  игровой таймер
 			switch (k)
 			{
 				case 5:
-					$('#b').html('<a href="javascript:success()" class="b">черный</a><br /><a href="#" class="b">синий</a><br /><a href="#" class="b">красный</a>');
+					$('#b').html('<a href="#" id="success" class="b">черный</a><br /><a href="#" class="b">синий</a><br /><a href="#" class="b">красный</a>');
 					break;
 				case 4:
-					$('#b').html('<a href="#" class="g">черный</a><br /><a href="javascript:success()" class="bl">синий</a><br /><a href="#" class="b">зеленый</a>');
+					$('#b').html('<a href="#" class="g">черный</a><br /><a href="#" id="success" class="bl">синий</a><br /><a href="#" class="b">зеленый</a>');
 					break;
 				case 3:
-					$('#b').html('<a href="#" class="g">красный</a><br /><a href="#" class="b">синий</a><br /><a href="javascript:success()" class="g">зеленый</a>');
+					$('#b').html('<a href="#" class="g">красный</a><br /><a href="#" class="b">синий</a><br /><a href="#" id="success" class="g">зеленый</a>');
 					break;
 				case 2:
-					$('#b').html('<a href="javascript:success()" class="bl">синий</a><br /><a href="#" class="b">красный</a><br /><a href="#" class="bl">зеленый</a>');
+					$('#b').html('<a href="#" id="success" class="bl">синий</a><br /><a href="#" class="b">красный</a><br /><a href="#" class="bl">зеленый</a>');
 					break;
 				case 1:
-					$('#b').html('<a href="" class="b">синий</a><br /><a href="javascript:success()" class="r">красный</a><br /><a href="#" class="bl">черный</a>');
+					$('#b').html('<a href="#" class="b">синий</a><br /><a href="#" id="success" class="r">красный</a><br /><a href="#" class="bl">черный</a>');
 					break;
 				default:    //  закончились вопросы
 					resume(); //    вызвать подведение итогов
 					break;
 			}
+			$('#success').one('click',success);
 			$('a').one('click',state);      //  выбрать все тэги а и по клику вызвать state
 		}
         
@@ -70,11 +81,14 @@ var timer,              //  игровой таймер
         
 	function resume()   //  функция вывода результатов
 		{
-			$('#b').html('<p>Вы набрали ' + numOfSuccess + ' очков из 5</p><a href="javascript:Main()">Играть снова</a>');
+			$('#b').html('<p>Вы набрали ' + numOfSuccess + ' очков из 5</p><a href="#">Играть снова</a>');
+			run();
 		}
         
 	function state(eventObj)
         {
+			//(!swither) ? null :swither = 1;
+			$('#success').unbind('click',success);	//	не давать засчитать ответ
 			$(this).toggleClass('y');           //  высветить ответ желтым
 			$('a').unbind('click',state);       //  и запретить повторный вызов этой функции
 		}
